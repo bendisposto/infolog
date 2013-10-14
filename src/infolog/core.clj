a5096aaff1728d6fa816677b4665e8ae45a0e270
Jens Bendisposto <jens@bendisposto.de>
2013-10-15 01:59:31 +0200

    refactored db code, not yet working

--- src/infolog/core.clj
+++ src/infolog/core.clj
@@ -2,8 +2,7 @@
   (:import infolog.PrologLexer)
   (:import java.io.ByteArrayInputStream)
   (:require [clojure.java.shell :refer [sh]]
-            [infolog.datomic :refer [schema]]
-            [datomic.api :as d]))
+            [infolog.datomic :as db]))
 
 (defn transform-comments [token]
   (let [text (.getText token)
@@ -24,7 +23,7 @@
 (def prolog-data (atom nil))
 ;(def uri "datomic:mem://infolog")
 (def uri "datomic:free://localhost:4334/infolog22")
-(def conn (atom nil))
+
 
 (defn extract-comments [filename]
   (when-not (.startsWith filename "sicstus/") (let [comments (PrologLexer/lexFile filename)]
@@ -48,112 +47,20 @@
         comments (into {} (filter (comp seq second) (map extract-comments (map :file (:modules data)))))]
     [comments data]))
 
-(defn ensure-schema [conn]
-  (or (-> conn d/db (d/entid :module/name))
-      @(d/transact conn schema)))
-
-(defn ensure-db [db-uri]
-  (let [newdb? (d/create-database db-uri)
-        c (d/connect db-uri)]
-    (ensure-schema c)
-    (reset! conn c)))
-
-(defn mk-module-tx [{:keys [m file]}]
-  (let [id (d/tempid :db.part/user)]
-    [[:db/add id :module/name m]
-     [:db/add id :module/file file]]))
-
-(defn mk-dependency-tx [conn {:keys [local_module imported_module]}]
-  (let [local (ffirst (d/q '[:find ?e :in $ ?name :where [?e :module/name ?name]] (d/db conn) local_module))
-        imported (ffirst (d/q '[:find ?e :in $ ?name :where [?e :module/name ?name]] (d/db conn) imported_module))]
-    [[:db/add local :module/dependson imported]]))
-
-(defn mk-predicate-tx [conn {:keys [m p a problem] :as all}]
-  (println "foo " all)
-  (let [id (d/tempid :db.part/user)
-        module (ffirst (d/q '[:find ?e :in $ ?name :where [?e :module/name ?name]] (d/db conn) m))]
-    (if module
-      [[:db/add id :predicate/module module]
-       [:db/add id :predicate/name p]
-       [:db/add id :predicate/arity a]]
-      (let [m (d/tempid :db.part/user)
-            base [[:db/add m :module/name module]
-                  [:db/add m :module/file (str "unknown/" module ".pl")]
-                  [:db/add m :prolog/problem true]
-                  [:db/add id :predicate/module m]
-                  [:db/add id :predicate/name p]
-                  [:db/add id :predicate/arity a]
-                  ]]
-        (if problem (conj base [:db/add id :prolog/problem true]) base)))))
-
-(defn find-predicate [conn m p a]
-  (println "fp" m p a)
-  (or (ffirst (d/q '[:find ?e :in $ ?module ?name ?arity :where
-                     [?e :predicate/name ?name]
-                     [?e :predicate/arity ?arity] 
-                     [?e :predicate/module ?m]
-                     [?m :module/name ?module]
-                     ] (d/db conn) m p a))
-      (do
-        (println "bug1")
-        @(d/transact conn (mk-predicate-tx conn {:m m :p p :a a :problem true}))
-        (ffirst (d/q '[:find ?e :in $ ?module ?name ?arity :where
-                       [?e :predicate/name ?name]
-                       [?e :predicate/arity ?arity] 
-                       [?e :predicate/module ?m]
-                       [?m :module/name ?module]
-                       ] (d/db conn) m p a)))))
-
-(defn mk-operator-tx [conn {:keys [m p a prio fix associativity]}]
-  (let [id (find-predicate conn m p a)
-        base [ [:db/add id :operator/fix fix]
-               [:db/add id :operator/priority prio]]]
-    (if (= "infix" fix) (conj base [:db/add id :operator/accociativity associativity]) base)))
-
-(defn mk-mode-tx [conn k {:keys [m p a args]}]
-  [[:db/add (find-predicate conn m p a)  k args]])
-
-(defn mk-bool-tx [conn k {:keys [m p a]}]
-  [[:db/add (find-predicate conn m p a)  k true]])
-
-(defn mk-call-tx [conn {:keys [m p a cm cp ca]}]
-  (let [caller (find-predicate conn m p a)
-        callee (find-predicate conn cm cp ca)]
-    [[:db/add caller :predicate/call callee]]))
-
-(defn mk-clause-tx [conn {:keys [m p a start end]}]
-  (let [id (d/tempid :db.part/user)
-        pid (find-predicate conn m p a)]
-    [[:db/add id :clause/predicate pid]
-     [:db/add id :clause/start start]
-     [:db/add id :clause/end end]]))
-
-
-(defn populate-db! [conn prolog-data]
-  (let [commit (ffirst (d/q '[:find ?e ?c :where [?e :git/commit ?c]] (d/db conn)))]
-    (when-not commit
-      @(d/transact conn (let [id (d/tempid :db.part/user)] [[:db/add id :module/name "user"] [:db/add id :module/file "sicstus/user.pl"]]))
-      @(d/transact conn (let [id (d/tempid :db.part/user)] [[:db/add id :module/name "built_in"] [:db/add id :module/file "sicstus/built_in.pl"]]))
-      @(d/transact conn (let [id (d/tempid :db.part/user)] [[:db/add id :module/name "undefined_module"] [:db/add id :module/file "sicstus/undefined_module.pl"]]))
-      @(d/transact conn [[:db/add (d/tempid :db.part/user) :git/commit (:git prolog-data)]])
-      @(d/transact conn (mapcat mk-module-tx (:modules prolog-data)))
-      @(d/transact conn (mapcat (partial mk-dependency-tx conn) (:dependencies prolog-data)))
-      @(d/transact conn (mapcat (partial mk-predicate-tx conn) (:predicates prolog-data)))
-      @(d/transact conn (mapcat (partial mk-bool-tx conn :predicate/dynamic) (:dynamic prolog-data)))
-      @(d/transact conn (mapcat (partial mk-bool-tx conn :predicate/volatile) (:volatile prolog-data)))
-      @(d/transact conn (mapcat (partial mk-bool-tx  conn :predicate/multifile) (:multifile prolog-data)))
-      @(d/transact conn (mapcat (partial mk-bool-tx  conn :predicate/exported) (:exported prolog-data)))
-      @(d/transact conn (mapcat (partial mk-mode-tx  conn :predicate/mode) (:mode prolog-data)))
-      @(d/transact conn (mapcat (partial mk-mode-tx  conn :predicate/meta) (:meta prolog-data)))
-      @(d/transact conn (mapcat (partial mk-mode-tx  conn :predicate/block) (:blocking prolog-data)))
-      @(d/transact conn (mapcat (partial mk-operator-tx  conn) (:operators prolog-data)))
-      @(d/transact conn (mapcat (partial mk-call-tx  conn) (:calling prolog-data)))
-      @(d/transact conn (mapcat (partial mk-clause-tx  conn) (:clauses prolog-data))))))
 
 (defn go [x]
   (let [uri (str "datomic:free://localhost:4334/infolog" x)]
     (read-prolog-info!)
-    (ensure-db uri)
-    (populate-db! @conn @prolog-data))
+    (db/ensure-db uri)
+    (db/populate-db! @prolog-data))
   :done)
+
+(defn go-mem []
+  (let [uri "datomic:mem://infolog"]
+    (db/delete-database uri)
+    (read-prolog-info!)
+    (db/ensure-db uri)
+    (db/populate-db! @prolog-data))
+  :done)
+
 
--- src/infolog/datomic.clj
+++ src/infolog/datomic.clj
@@ -1,6 +1,9 @@
 (ns infolog.datomic
     (:require [datomic.api :as d]))
 
+(def conn (atom nil))
+(def progresscount (atom 0))
+
 (def schema
   [
    {:db/id #db/id[:db.part/db]
@@ -173,3 +176,137 @@
     :db.install/_attribute :db.part/db }
    
    ])
+
+
+(defn ensure-schema [conn]
+  (or (-> conn d/db (d/entid :module/name))
+      @(d/transact conn schema)))
+
+(def tempid? map?)
+
+(defn ensure-db [db-uri]
+  (let [newdb? (d/create-database db-uri)
+        c (d/connect db-uri)]
+    (ensure-schema c)
+    (reset! conn c)))
+
+(defn mk-module-tx [{:keys [m file]}]
+  (let [id (d/tempid :db.part/user)]
+    [[:db/add id :module/name m]
+     [:db/add id :module/file file]]))
+
+(defn mk-dependency-tx [conn {:keys [local_module imported_module]}]
+  (let [local (ffirst (d/q '[:find ?e :in $ ?name :where [?e :module/name ?name]] (d/db conn) local_module))
+        imported (ffirst (d/q '[:find ?e :in $ ?name :where [?e :module/name ?name]] (d/db conn) imported_module))]
+    [[:db/add local :module/dependson imported]]))
+
+
+(defn find-module
+  "Retrieves the module id from the database. If it does not yet exists, the module will be created and marked as a problem."
+  [conn m]
+  (let [module (or (ffirst (d/q '[:find ?e :in $ ?name :where [?e :module/name ?name]] (d/db conn) m))
+                   (d/tempid :db.part/user))]
+    (if (tempid? module)
+      (do (println "Missing module: " m)
+          @(d/transact conn
+                         [[:db/add m :module/name module]
+                          [:db/add m :module/file (str "unknown/" module ".pl")]
+                          [:db/add m :prolog/problem true]])
+          (find-module conn m))
+      module)))
+
+(defn mk-predicate-tx
+  ([conn {:keys [m p a]}]
+     (let [module (find-module conn m)]
+       (mk-predicate-tx module p a false)))
+  ([module p a] (mk-predicate-tx module p a false))
+  ([module p a problem?]
+     (let [id (d/tempid :db.part/user)
+           default [[:db/add id :predicate/module module]
+                    [:db/add id :predicate/name p]
+                    [:db/add id :predicate/arity a]]]
+       (if problem? (conj default [:db/add id :prolog/problem true])))))
+
+(defn find-predicate [conn m p a]
+  (let [module (find-module conn m)
+        predicate (or  (ffirst (d/q '[:find ?e :in $ ?module ?name ?arity :where
+                                      [?e :predicate/name ?name]
+                                      [?e :predicate/arity ?arity] 
+                                      [?e :predicate/module ?module]]
+                                    (d/db conn) module p a))
+                       (d/tempid :db.part/user))]
+    (if (tempid? predicate)
+      (do
+        (println "Missing predicate " m ":" p "/" a)
+        @(d/transact conn (mk-predicate-tx module p a true))
+        (find-predicate conn m p a))
+      predicate)))
+
+
+
+(defn mk-operator-tx [conn {:keys [m p a prio fix associativity]}]
+  (let [id (find-predicate conn m p a)
+        base [ [:db/add id :operator/fix fix]
+               [:db/add id :operator/priority prio]]]
+    (if (= "infix" fix) (conj base [:db/add id :operator/accociativity associativity]) base)))
+
+(defn mk-mode-tx [conn k {:keys [m p a args]}]
+  [[:db/add (find-predicate conn m p a)  k args]])
+
+(defn mk-bool-tx [conn k {:keys [m p a]}]
+  (let [id (find-predicate conn m p a)]
+    (println "Setting " k " for " id " - " m ":" p "/" a)
+    [[:db/add id k true]]))
+
+(defn mk-call-tx [conn {:keys [m p a cm cp ca]}]
+  (let [caller (find-predicate conn m p a)
+        callee (find-predicate conn cm cp ca)]
+    (swap! progresscount inc)
+    (when (== 0 (mod @progresscount 1000)) (println @progresscount))
+    [[:db/add caller :predicate/call callee]]))
+
+(defn mk-clause-tx [conn {:keys [m p a start end]}]
+  (let [id (d/tempid :db.part/user)
+        pid (find-predicate conn m p a)]
+    (swap! progresscount inc)
+    (when (== 0 (mod @progresscount 1000)) (println @progresscount))
+    [[:db/add id :clause/predicate pid]
+     [:db/add id :clause/start start]
+     [:db/add id :clause/end end]]))
+
+(defn delete-database [uri] (d/delete-database uri))
+
+(defn populate-db! [prolog-data]
+  (let [conn @conn
+        commit (ffirst (d/q '[:find ?e ?c :where [?e :git/commit ?c]] (d/db conn)))]
+    (when-not commit
+      @(d/transact conn (let [id (d/tempid :db.part/user)] [[:db/add id :module/name "user"] [:db/add id :module/file "sicstus/user.pl"]]))
+      @(d/transact conn (let [id (d/tempid :db.part/user)] [[:db/add id :module/name "built_in"] [:db/add id :module/file "sicstus/built_in.pl"]]))
+      @(d/transact conn (let [id (d/tempid :db.part/user)] [[:db/add id :module/name "undefined_module"] [:db/add id :module/file "sicstus/undefined_module.pl"]]))
+      @(d/transact conn [[:db/add (d/tempid :db.part/user) :git/commit (:git prolog-data)]])
+      @(d/transact conn (mapcat mk-module-tx (:modules prolog-data)))
+      @(d/transact conn (mapcat (partial mk-dependency-tx conn) (:dependencies prolog-data)))
+      (println "Processing predicates (" (count (:predicates prolog-data)) ")")
+      @(d/transact conn (mapcat (partial mk-predicate-tx conn) (:predicates prolog-data)))
+      (println "Processing predicate attribute: " "dynamic")
+      @(d/transact conn (mapcat (partial mk-bool-tx conn :predicate/dynamic) (:dynamic prolog-data)))
+      (println "Processing predicate attribute: " "volatile")
+      @(d/transact conn (mapcat (partial mk-bool-tx conn :predicate/volatile) (:volatile prolog-data)))
+      (println "Processing predicate attribute: " "multifile")
+      @(d/transact conn (mapcat (partial mk-bool-tx  conn :predicate/multifile) (:multifile prolog-data)))
+      (println "Processing predicate attribute: " "exported")     
+      @(d/transact conn (mapcat (partial mk-bool-tx  conn :predicate/exported) (:exported prolog-data)))
+      (println "Processing predicate attribute: " "mode")
+      @(d/transact conn (mapcat (partial mk-mode-tx  conn :predicate/mode) (:mode prolog-data)))
+      (println "Processing predicate attribute: " "meta")
+      @(d/transact conn (mapcat (partial mk-mode-tx  conn :predicate/meta) (:meta prolog-data)))
+      (println "Processing predicate attribute: " "block")
+      @(d/transact conn (mapcat (partial mk-mode-tx  conn :predicate/block) (:blocking prolog-data)))
+      (println "Processing operators")
+      @(d/transact conn (mapcat (partial mk-operator-tx  conn) (:operators prolog-data)))
+      (println "Processing calls (" (count (:calling prolog-data)) ")")
+      @(d/transact conn (mapcat (partial mk-call-tx  conn) (:calling prolog-data)))
+      (println "Processing clauses (" (count (:clauses prolog-data)) ")")
+      (reset! progresscount 0)
+      @(d/transact conn (mapcat (partial mk-clause-tx  conn) (:clauses prolog-data))))))
+
