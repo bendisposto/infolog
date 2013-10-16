ca377bbf5aa1277be9c3eb39340c91b44544b0d3
Jens Bendisposto <jens@bendisposto.de>
2013-10-16 12:33:21 +0200

    no idea

--- src/infolog/datomic.clj
+++ src/infolog/datomic.clj
@@ -1,181 +1,10 @@
 (ns infolog.datomic
-    (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]))
 
 (def conn (atom nil))
 (def progresscount (atom 0))
+(declare schema)
 
-(def schema
-  [
-   {:db/id #db/id[:db.part/db]
-    :db/ident :git/commit
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Git revision"
-    :db.install/_attribute :db.part/db}
-   
-   {:db/id #db/id[:db.part/db]
-    :db/ident :module/name
-    :db/fulltext true
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Module name"
-    :db.install/_attribute :db.part/db}
-   
-   {:db/id #db/id[:db.part/db]
-    :db/ident :module/file
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "File containing the module"
-    :db.install/_attribute :db.part/db}
-   
-   {:db/id #db/id[:db.part/db]
-    :db/ident :module/dependson
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "Module dependency"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :prolog/problem
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/many
-    :db/doc "indicates that this definition was added by database import"
-    :db.install/_attribute :db.part/db}
-   
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/module
-    :db/index true
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "Module containing predicate"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/name
-    :db/fulltext true
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "Predictate name"
-    :db.install/_attribute :db.part/db}
-
-
-
-   
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/arity
-    :db/index true
-    :db/valueType :db.type/long
-    :db/cardinality :db.cardinality/one
-    :db/doc "Predictate arity"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/dynamic
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/one
-    :db/doc "true iff the predicate is dynamic"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/volatile
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/one
-    :db/doc "true iff the predicate is volatile"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/multifile
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/one
-    :db/doc "true if the predicate is defined across multiple files"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/exported
-    :db/valueType :db.type/boolean
-    :db/cardinality :db.cardinality/one
-    :db/doc "true iff the predicate is exported"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/mode
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/many
-    :db/doc "mode declaration"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/meta
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/many
-    :db/doc "meta declaration"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/block
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/many
-    :db/doc "blocking declaration"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :operator/fix
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "prefix, postfix, infix"
-    :db.install/_attribute :db.part/db}
-   
-   {:db/id #db/id[:db.part/db]
-    :db/ident :operator/priority
-    :db/valueType :db.type/long
-    :db/cardinality :db.cardinality/one
-    :db/doc "operator priority"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :operator/accociativity
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "accociativity of an infix operator: left, right, not"
-    :db.install/_attribute :db.part/db }
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :clause/predicate
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/doc "reference to the predicate containing a clause"
-    :db.install/_attribute :db.part/db }
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :clause/start
-    :db/valueType :db.type/long
-    :db/cardinality :db.cardinality/one
-    :db/doc "line number where the clause starts"
-    :db.install/_attribute :db.part/db }
-   
-   {:db/id #db/id[:db.part/db]
-    :db/ident :clause/end
-    :db/valueType :db.type/long
-    :db/cardinality :db.cardinality/one
-    :db/doc "line number where the clause ends"
-    :db.install/_attribute :db.part/db }
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/call
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc "reference to called predicate"
-    :db.install/_attribute :db.part/db }
-
-   {:db/id #db/id[:db.part/db]
-    :db/ident :predicate/doc
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc "documentation"
-    :db.install/_attribute :db.part/db }
-   
-   ])
 
 
 (defn ensure-schema [conn]
@@ -208,31 +37,31 @@
                    (d/tempid :db.part/user))]
     (if (tempid? module)
       (do (println "Missing module: " m)
-          @(d/transact conn
-                         [[:db/add m :module/name module]
-                          [:db/add m :module/file (str "unknown/" module ".pl")]
-                          [:db/add m :prolog/problem true]])
-          (find-module conn m))
+        @(d/transact conn
+                     [[:db/add m :module/name module]
+                      [:db/add m :module/file (str "unknown/" module ".pl")]
+                      [:db/add m :prolog/problem true]])
+        (find-module conn m))
       module)))
 
 (defn mk-predicate-tx
   ([conn {:keys [m p a]}]
-     (let [module (find-module conn m)]
-       (mk-predicate-tx module p a false)))
+    (let [module (find-module conn m)]
+      (mk-predicate-tx module p a false)))
   ([module p a] (mk-predicate-tx module p a false))
   ([module p a problem?]
-     (let [id (d/tempid :db.part/user)
-           default [[:db/add id :predicate/module module]
-                    [:db/add id :predicate/name p]
-                    [:db/add id :predicate/arity a]]]
-       (if problem? (conj default [:db/add id :prolog/problem true])))))
+    (let [id (d/tempid :db.part/user)
+          default [[:db/add id :predicate/module module]
+                   [:db/add id :predicate/name p]
+                   [:db/add id :predicate/arity a]]]
+      (if problem? (conj default [:db/add id :prolog/problem true])))))
 
 (defn find-predicate [conn m p a]
   (let [module (find-module conn m)
         predicate (or  (ffirst (d/q '[:find ?e :in $ ?module ?name ?arity :where
-                                      [?e :predicate/name ?name]
+                                      [[?e :predicate/name ?name]
                                       [?e :predicate/arity ?arity] 
-                                      [?e :predicate/module ?module]]
+                                      [?e :predicate/module ?module]]]
                                     (d/db conn) module p a))
                        (d/tempid :db.part/user))]
     (if (tempid? predicate)
@@ -247,7 +76,7 @@
 (defn mk-operator-tx [conn {:keys [m p a prio fix associativity]}]
   (let [id (find-predicate conn m p a)
         base [ [:db/add id :operator/fix fix]
-               [:db/add id :operator/priority prio]]]
+              [:db/add id :operator/priority prio]]]
     (if (= "infix" fix) (conj base [:db/add id :operator/accociativity associativity]) base)))
 
 (defn mk-mode-tx [conn k {:keys [m p a args]}]
@@ -255,7 +84,7 @@
 
 (defn mk-bool-tx [conn k {:keys [m p a]}]
   (let [id (find-predicate conn m p a)]
-    (println "Setting " k " for " id " - " m ":" p "/" a)
+    ;(println "Setting " k " for " id " - " m ":" p "/" a)
     [[:db/add id k true]]))
 
 (defn mk-call-tx [conn {:keys [m p a cm cp ca]}]
@@ -309,4 +138,176 @@
       (println "Processing clauses (" (count (:clauses prolog-data)) ")")
       (reset! progresscount 0)
       @(d/transact conn (mapcat (partial mk-clause-tx  conn) (:clauses prolog-data))))))
+
+
+
+(def schema
+  [
+   {:db/id #db/id[:db.part/db]
+    :db/ident :git/commit
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Git revision"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :module/name
+    :db/fulltext true
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Module name"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :module/file
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "File containing the module"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :module/dependson
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/doc "Module dependency"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :prolog/problem
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/many
+    :db/doc "indicates that this definition was added by database import"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/module
+    :db/index true
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "Module containing predicate"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/name
+    :db/fulltext true
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Predictate name"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/arity
+    :db/index true
+    :db/valueType :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc "Predictate arity"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/dynamic
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc "true iff the predicate is dynamic"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/volatile
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc "true iff the predicate is volatile"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/multifile
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc "true if the predicate is defined across multiple files"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/exported
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc "true iff the predicate is exported"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/mode
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/many
+    :db/doc "mode declaration"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/meta
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/many
+    :db/doc "meta declaration"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/block
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/many
+    :db/doc "blocking declaration"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :operator/fix
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "prefix, postfix, infix"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :operator/priority
+    :db/valueType :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc "operator priority"
+    :db.install/_attribute :db.part/db}
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :operator/accociativity
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "accociativity of an infix operator: left, right, not"
+    :db.install/_attribute :db.part/db }
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :clause/predicate
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "reference to the predicate containing a clause"
+    :db.install/_attribute :db.part/db }
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :clause/start
+    :db/valueType :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc "line number where the clause starts"
+    :db.install/_attribute :db.part/db }
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :clause/end
+    :db/valueType :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc "line number where the clause ends"
+    :db.install/_attribute :db.part/db }
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/call
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/doc "reference to called predicate"
+    :db.install/_attribute :db.part/db }
+   
+   {:db/id #db/id[:db.part/db]
+    :db/ident :predicate/doc
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "documentation"
+    :db.install/_attribute :db.part/db }
+   
+   ])
+
 
