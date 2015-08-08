f28486ac83732c81dad1fbdfcc7319efd67ca82b
Jens Bendisposto <jens@bendisposto.de>
2015-08-09 01:15:07 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "https://github.com/bendisposto/infolog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["cobra" "http://cobra.cs.uni-duesseldorf.de/artifactory/repo"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.logic "0.8.10"]
-                 [infolog/doc_extractor "0.0.1-20131008.113111-1"]
+                 [de.hhu.stups/infolog-parser "0.1.0"]
                  [org.clojure/data.zip "0.1.1"]]
                  :main infolog.core)
