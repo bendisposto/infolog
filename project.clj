d3f3bfbd7bcca26d5a0d6700573a230b90e8eb55
Jens Bendisposto <jens@bendisposto.de>
2015-09-01 10:49:17 +0200

    start work on frontend

--- project.clj
+++ project.clj
@@ -4,17 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.logic "0.8.10"]
                  [de.hhu.stups/infolog-parser "0.1.0"]
-                 [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/data.csv "0.1.3"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
                  [org.clojure/data.json "0.2.6"]
                  [ring/ring-defaults "0.1.5"]]
   :plugins [[lein-ring "0.9.6"]]
   :ring {:handler infolog.core/app
-         :nrepl {:start? tr}}
-
+         :nrepl {:start? true}}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}}
--- src/infolog/core.clj
+++ src/infolog/core.clj
@@ -1,53 +1,15 @@
 (ns infolog.core
   (:refer-clojure :exclude [==])
-  (:require [clojure.edn :as edn]
-            [clojure.java.shell :refer [sh]]
-            [clojure.core.logic.fd :as fd]
+  (:require [clojure.java.shell :refer [sh]]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.handler :as handler]
             [hiccup.middleware :refer (wrap-base-url)]
             [hiccup.core :as h]
-            [clojure.data.json :as json])
-  (:import infolog.PrologLexer java.io.ByteArrayInputStream)
-  (:use clojure.core.logic
-        clojure.core.logic.pldb))
-
-(def entry-point "/src/prob_tcltk.pl")
-(def db-file "database.clj")
-
-(def facts (atom {}))
-
-(db-rel git sha)
-(db-rel dependency importing-module imported-module)
-(db-rel module module-name file)
-(db-rel problem description)
-(db-rel clause module-name predicate-name arity ^:index start end)
-(db-rel operator module-name predicate-name arity priority fix associativity)
-(db-rel call caller-module caller-predicate caller-arity callee-module callee-predicate callee-arity start end)
-(db-rel predicate module predicate-name arity)
-(db-rel is_dynamic module predicate-name arity)
-(db-rel is_volatile module predicate-name arity)
-(db-rel is_exported module predicate-name arity)
-(db-rel is_multifile module predicate-name arity)
-(db-rel is_meta module predicate-name arity args)
-(db-rel declared_mode module predicate-name arity args)
-(db-rel is_blocking module predicate-name arity args)
-(db-rel documentation module predicate-name arity text)
-
-(defn predicates-after-line [db m l]
-  (with-db db
-    (run* [q]
-      (fresh [p a s e d]
-        (clause m p a s e)
-        (fd/< l s)
-        (== q [p a s])))))
-
-(defn comment->predicate [db module line]
-  (->> (predicates-after-line db module line)
-       (sort-by last)
-       first
-       butlast))
+            [clojure.data.json :as json]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io])
+  (:import infolog.PrologLexer java.io.ByteArrayInputStream))
 
 (defn transform-comments [module token]
   (let [text (.getText token)
@@ -57,97 +19,62 @@
         end-column (-> token .getEndPos .getPos)]
     [module text start-line end-line]))
 
-(defn assert-comment [db [m text start-line end-line]]
-  (let [[p a] (comment->predicate db m end-line)]
-    (-> db (db-fact documentation m p a text))))
-
 (defn extract-comments [[m file]]
   (when-not (.startsWith file "sicstus/")
     (let [comments (PrologLexer/lexFile file)]
       (map (partial transform-comments m) comments))))
 
-(defn sicstus-call [prob-home output-file]
+#_(defn sicstus-call [prob-home output-file]
   ["sicstus" "-l" "./prolog-analyzer/analyzer.pl"
    "--goal"
    (str "analyze('" prob-home entry-point "','" output-file "').")])
 
-(defn run-prolog-analyzer [prob-home filename]
+#_(defn run-prolog-analyzer [prob-home filename]
   (apply sh (sicstus-call prob-home filename))
   filename)
 
 (defn get-comments [modules]
   (mapcat extract-comments modules))
 
-(defn- load-pl-output [f] (edn/read-string (slurp f)))
-(defn- eval-all [c] (doall (map eval c)))
-(defn- mk-db [facts] (apply db facts))
-
-(defn make-db [input-file output-file]
-  (let [facts (->> input-file load-pl-output eval-all mk-db)
-        mods (with-db facts (run* [q] (fresh [m f] (module m f) (== q [m f]))))
-        comments (get-comments mods)]
-    (spit output-file facts)
-    (spit "comments.clj" (pr-str  comments))))
-
-(defn load-database [file]
-  (-> file
-      slurp
-      edn/read-string))
-
-(defn noto [g]
-  (fn [a]
-    (if (nil? (g a))
-      a)))
 
 (def sicstus-module #{"lists" "avl" "file_systems" "codesio" "ordsets" "timeout"
                       "chr" "clpfd" "system" "between" "samsort" "random" "atts"
                       "terms" "sets" "gauge" "trees" "assoc" "xml" "process"
                       "aggregate" "tcltk" "heaps" })
 
-(defn modules [f]
-  (with-db f (run* [q] (fresh [f] (module q f)))))
-
-(defn dependencies [f]
-  (with-db f (run* [q] (fresh [m d] (dependency d m) (== q [d m])))))
-
-
-(defn dependency-graph [f]
-  (str "digraph dependencies {\n"
-       (clojure.string/join "," (map (fn [e] (str \" e \")) (take 5 (modules f))))
-       (clojure.string/join
-        "\n"
-        (map (fn [[d m]] (str \" d \" "->" \" m \")) (take 500 (dependencies f)))) "}"))
-
-(defn fix-module [m]
-  (clojure.string/join "_" (clojure.string/split m #"/")))
-
-(defn mk-dep [f]
-  (let [m-dep (into {} (remove sicstus-module (map (fn [m] [(fix-module m) []]) (modules f))))
-        g-dep (group-by first (dependencies f))
-        t-dep (remove nil? (map (fn [[k v]] (when (not (sicstus-module k)) [(fix-module k) (remove nil? (mapv (fn [[_ m]] (when (not (sicstus-module m)) (fix-module m))) v))])) (merge m-dep g-dep)))]
-    (map (fn [[k v]] {:name k :size (* 1000 (count v)) :imports v}) t-dep)))
-
-(defn details [f m]
-  (let [imports (with-db f (run* [q] (fresh [d] (dependency  m d) (== q d))))
-        imported-by (with-db f (run* [q] (fresh [d] (dependency  d m) (== q d))))]
-    {:name m :imports imports :imported_by imported-by}))
-
-(defn patho [n m]
-  (fresh [x] (dependency n x) (patho x m)))
-
-(defn cyclic-dependencies [f]
-  (with-db f (run* [q] (fresh [m f] (module m f) (patho m m) (== q m)))))
-
 (defroutes app-routes
-  (GET "/" [] (h/html [:h1 "Infolog"] [:a {:href "dependencies.html"} "Module Dependency Visualization"]))
-  (GET "/dependencies" [] (json/write-str (mk-dep @facts)))
-  (GET "/cycles" [] (json/write-str (cyclic-dependencies @facts)))
-  (GET "/dependency-details/:module" [module] (json/write-str (details @facts module)))
+  (GET "/" [] (h/html [:h1 "Infolog"]
+     #_[:a {:href "dependencies.html"} "Module Dependency Visualization"]
+       [:a {:href "problems.html"} "linter-Results"]))
+       (GET "problemlist" [] (json/write-str []))
+  #_(GET "/dependencies" [] (json/write-str (mk-dep @facts)))
+  #_(GET "/cycles" [] (json/write-str (cyclic-dependencies @facts)))
+  #_(GET "/dependency-details/:module" [module] (json/write-str (details @facts module)))
   (route/resources "/"))
 
-(def app (do (reset! facts (load-database "database.clj"))
-             (-> (handler/site app-routes)
-                 (wrap-base-url))))
+(def app (do (-> (handler/site app-routes)
+             (wrap-base-url))))
+
+
+(defn start-prolog-repl []
+  (let [p (shell/proc "sicstus" "-i" "-l" "prolog-analyzer/analyzer.pl" :redirect-err true)
+        wrt (java.io.OutputStreamWriter. (:in p))
+        rdr (java.io.InputStreamReader. (:out p))]
+    (.write wrt "repl.\n")
+    (.flush wrt)
+    (assoc p :writer wrt :reader rdr)))
+
+(defn q [sicstus query]
+  (let [w (:writer sicstus)
+        r (:reader sicstus)]
+    (doto w
+      (.write query)
+      (.write "\n")
+      (.flush))
+    ;;(println (shell/read-line sicstus :out))
+    ;; read-result
+    ;; process result
+    ))
 
 
 (comment
