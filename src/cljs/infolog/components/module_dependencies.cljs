ee34f5f4dfe0053945001ac7dbce85108a283abe
Jens Bendisposto <jens@bendisposto.de>
2015-09-08 09:55:19 +0200

    basic dependency visualization

--- src/cljs/infolog/components/module_dependencies.cljs
+++ src/cljs/infolog/components/module_dependencies.cljs
@@ -5,6 +5,27 @@
             [taoensso.encore :as enc  :refer (logf log logp)]
             [depsd3]))
 
+(def cz 4)
+
+(defn compute-color [deps modules x y]
+  (let [m1 (get modules x)
+        m2 (get modules y)]
+    (cond (= m1 m2) "white"
+          (and (get deps [m1 m2]) (get deps [m2 m1])) "red"
+          (get deps [m1 m2]) "green"
+          (get deps [m2 m1]) "blue"
+          :else "white")))
 
 (defn dependency-graph []
-  [:div "D"])
+  (let [deps (re-frame/subscribe [:dependencies])
+        modules (reaction (into [] @(re-frame/subscribe [:modules identity])))
+        size (reaction (count @modules))]
+    [:svg {:height (* @size cz) :width (* @size cz)}
+     (doall (for [x (range 0 @size) y (range 0 @size)]
+              [:rect {:key (str x "," y)
+                      :x (* x cz)
+                      :y (* y cz)
+                      :width cz
+                      :height cz
+                      :fill (compute-color @deps @modules x y)}]))]
+    ))
